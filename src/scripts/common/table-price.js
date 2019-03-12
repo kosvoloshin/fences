3b424d9ae16f8e8082f174b1ce7dce34be1a46f1
Konstantin <23.03@i.ua>
2019-03-12 19:13:58 +0200

    fix coding

--- src/scripts/common/table-price.js
+++ src/scripts/common/table-price.js
@@ -1,35 +1,40 @@
-var accordion = document.getElementById("price"),
-    items = accordion.getElementsByClassName("price__item"),
-    contents = accordion.getElementsByClassName("price__content"),
-    i;
+const $ = require('jquery');
 
-accordion.addEventListener("click", function(e) {
-  if (e.target.classList.contains("price__trigger")) {
-    var trigger = e.target;
-    var content = trigger.nextElementSibling;
-    var item = trigger.parentNode;
+if($('#price').length){
 
-    if (!item.classList.contains("active")) { // не активный
-      // удаляем active для всех li
-      for (i = 0; i < items.length; i++) {
-        items[i].classList.remove("active");
+  var accordion = document.getElementById("price"),
+      items = accordion.getElementsByClassName("price__item"),
+      contents = accordion.getElementsByClassName("price__content"),
+      i;
+
+  accordion.addEventListener("click", function(e) {
+    if (e.target.classList.contains("price__trigger")) {
+      var trigger = e.target;
+      var content = trigger.nextElementSibling;
+      var item = trigger.parentNode;
+
+      if (!item.classList.contains("active")) { // не активный
+        // удаляем active для всех li
+        for (i = 0; i < items.length; i++) {
+          items[i].classList.remove("active");
+        }
+        // добавляем active для текущего li
+        item.classList.add("active");
+
+        // закрываем все блоки с контентом
+        for (i = 0; i < contents.length; i++) {
+          contents[i].style.height = null;
+        }
+        // открываем текущий блок с контентом
+        content.style.height = content.scrollHeight + "px";
+        
+      } else { // активный
+        // удаляем класс active для текущего li
+        item.classList.remove("active");
+        
+        // закрываем текущий блок с контентом
+        content.style.height = null;
       }
-      // добавляем active для текущего li
-      item.classList.add("active");
-
-      // закрываем все блоки с контентом
-      for (i = 0; i < contents.length; i++) {
-        contents[i].style.height = null;
-      }
-      // открываем текущий блок с контентом
-      content.style.height = content.scrollHeight + "px";
-      
-    } else { // активный
-      // удаляем класс active для текущего li
-      item.classList.remove("active");
-      
-      // закрываем текущий блок с контентом
-      content.style.height = null;
     }
-  }
-});
+  });
+}
